f9e424bafdd4d3a7fa14388c258d4d97aeea32d2
jianjungki <jianjungki@sina.com>
2024-07-01 13:52:00 +0800

    update math return

--- src/views/ocr/v3/math_enhance.vue
+++ src/views/ocr/v3/math_enhance.vue
@@ -19,7 +19,7 @@
           class="upload-demo"
           drag
           :disabled="disable"
-          action="https://api-internal.wefile.com/ocr/combine?format=docx"
+          action="https://api-internal.wefile.com/ocr/mix"
           :before-upload="beforeUpload"
           :on-progress="handleProgress"
           :on-error="handleError"
