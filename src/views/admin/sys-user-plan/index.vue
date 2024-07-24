e8652d5904b1482c2af7308901894ab173be8d66
jianjun <jianjungki@sina.com>
2024-07-24 17:05:48 +0800

    add plan and order for goadmin

---
+++ src/views/admin/sys-user-plan/index.vue
@@ -0,0 +1,344 @@
+
+<template>
+    <BasicLayout>
+        <template #wrapper>
+            <el-card class="box-card">
+                <el-form ref="queryForm" :model="queryParams" :inline="true" label-width="68px">
+                    
+                    <el-form-item>
+                        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+                        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+                    </el-form-item>
+                </el-form>
+
+                <el-row :gutter="10" class="mb8">
+                    <el-col :span="1.5">
+                        <el-button
+                                v-permisaction="['admin:sysUserPlan:add']"
+                                type="primary"
+                                icon="el-icon-plus"
+                                size="mini"
+                                @click="handleAdd"
+                        >新增
+                        </el-button>
+                    </el-col>
+                    <el-col :span="1.5">
+                        <el-button
+                                v-permisaction="['admin:sysUserPlan:edit']"
+                                type="success"
+                                icon="el-icon-edit"
+                                size="mini"
+                                :disabled="single"
+                                @click="handleUpdate"
+                        >修改
+                        </el-button>
+                    </el-col>
+                    <el-col :span="1.5">
+                        <el-button
+                                v-permisaction="['admin:sysUserPlan:remove']"
+                                type="danger"
+                                icon="el-icon-delete"
+                                size="mini"
+                                :disabled="multiple"
+                                @click="handleDelete"
+                        >删除
+                        </el-button>
+                    </el-col>
+                </el-row>
+
+                <el-table v-loading="loading" :data="sysUserPlanList" @selection-change="handleSelectionChange">
+                    <el-table-column type="selection" width="55" align="center"/>
+                    <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+                        <template slot-scope="scope">
+                         <el-popconfirm
+                           class="delete-popconfirm"
+                           title="确认要修改吗?"
+                           confirm-button-text="修改"
+                           @confirm="handleUpdate(scope.row)"
+                         >
+                           <el-button
+                             slot="reference"
+                             v-permisaction="['admin:sysUserPlan:edit']"
+                             size="mini"
+                             type="text"
+                             icon="el-icon-edit"
+                           >修改
+                           </el-button>
+                         </el-popconfirm>
+                         <el-popconfirm
+                            class="delete-popconfirm"
+                            title="确认要删除吗?"
+                            confirm-button-text="删除"
+                            @confirm="handleDelete(scope.row)"
+                         >
+                            <el-button
+                              slot="reference"
+                              v-permisaction="['admin:sysUserPlan:remove']"
+                              size="mini"
+                              type="text"
+                              icon="el-icon-delete"
+                            >删除
+                            </el-button>
+                         </el-popconfirm>
+                        </template>
+                    </el-table-column>
+                </el-table>
+
+                <pagination
+                        v-show="total>0"
+                        :total="total"
+                        :page.sync="queryParams.pageIndex"
+                        :limit.sync="queryParams.pageSize"
+                        @pagination="getList"
+                />
+
+                <!-- 添加或修改对话框 -->
+                <el-dialog :title="title" :visible.sync="open" width="500px">
+                    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+                        
+                                    <el-form-item label="" prop="id">
+                                        <el-input v-model="form.id" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="quota">
+                                        <el-input v-model="form.quota" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="used">
+                                        <el-input v-model="form.used" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="leave">
+                                        <el-input v-model="form.leave" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="userId">
+                                        <el-input v-model="form.userId" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="planId">
+                                        <el-input v-model="form.planId" placeholder=""
+                                                      />
+                                    </el-form-item>
+                                    <el-form-item label="" prop="startAt">
+                                        <el-date-picker
+                                                    v-model="form.startAt"
+                                                    type="datetime"
+                                                    placeholder="选择日期">
+                                            </el-date-picker>
+                                    </el-form-item>
+                                    <el-form-item label="" prop="updateAt">
+                                        <el-date-picker
+                                                    v-model="form.updateAt"
+                                                    type="datetime"
+                                                    placeholder="选择日期">
+                                            </el-date-picker>
+                                    </el-form-item>
+                                    <el-form-item label="" prop="createAt">
+                                        <el-date-picker
+                                                    v-model="form.createAt"
+                                                    type="datetime"
+                                                    placeholder="选择日期">
+                                            </el-date-picker>
+                                    </el-form-item>
+                                    <el-form-item label="" prop="endAt">
+                                        <el-date-picker
+                                                    v-model="form.endAt"
+                                                    type="datetime"
+                                                    placeholder="选择日期">
+                                            </el-date-picker>
+                                    </el-form-item>
+                                    <el-form-item label="" prop="deleteAt">
+                                        <el-date-picker
+                                                    v-model="form.deleteAt"
+                                                    type="datetime"
+                                                    placeholder="选择日期">
+                                            </el-date-picker>
+                                    </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button type="primary" @click="submitForm">确 定</el-button>
+                        <el-button @click="cancel">取 消</el-button>
+                    </div>
+                </el-dialog>
+            </el-card>
+        </template>
+    </BasicLayout>
+</template>
+
+<script>
+    import {addSysUserPlan, delSysUserPlan, getSysUserPlan, listSysUserPlan, updateSysUserPlan} from '@/api/admin/sys-user-plan'
+    
+    export default {
+        name: 'SysUserPlan',
+        components: {
+        },
+        data() {
+            return {
+                // 遮罩层
+                loading: true,
+                // 选中数组
+                ids: [],
+                // 非单个禁用
+                single: true,
+                // 非多个禁用
+                multiple: true,
+                // 总条数
+                total: 0,
+                // 弹出层标题
+                title: '',
+                // 是否显示弹出层
+                open: false,
+                isEdit: false,
+                // 类型数据字典
+                typeOptions: [],
+                sysUserPlanList: [],
+                
+                // 关系表类型
+                
+                // 查询参数
+                queryParams: {
+                    pageIndex: 1,
+                    pageSize: 10,
+                    
+                },
+                // 表单参数
+                form: {
+                },
+                // 表单校验
+                rules: {}
+        }
+        },
+        created() {
+            this.getList()
+            },
+        methods: {
+            /** 查询参数列表 */
+            getList() {
+                this.loading = true
+                listSysUserPlan(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+                        this.sysUserPlanList = response.data.list
+                        this.total = response.data.count
+                        this.loading = false
+                    }
+                )
+            },
+            // 取消按钮
+            cancel() {
+                this.open = false
+                this.reset()
+            },
+            // 表单重置
+            reset() {
+                this.form = {
+                
+                id: undefined,
+                quota: undefined,
+                used: undefined,
+                leave: undefined,
+                userId: undefined,
+                planId: undefined,
+                startAt: undefined,
+                updateAt: undefined,
+                createAt: undefined,
+                endAt: undefined,
+                deleteAt: undefined,
+            }
+                this.resetForm('form')
+            },
+            getImgList: function() {
+              this.form[this.fileIndex] = this.$refs['fileChoose'].resultList[0].fullUrl
+            },
+            fileClose: function() {
+              this.fileOpen = false
+            },
+            // 关系
+            // 文件
+            /** 搜索按钮操作 */
+            handleQuery() {
+                this.queryParams.pageIndex = 1
+                this.getList()
+            },
+            /** 重置按钮操作 */
+            resetQuery() {
+                this.dateRange = []
+                this.resetForm('queryForm')
+                this.handleQuery()
+            },
+            /** 新增按钮操作 */
+            handleAdd() {
+                this.reset()
+                this.open = true
+                this.title = '添加SysUserPlan'
+                this.isEdit = false
+            },
+            // 多选框选中数据
+            handleSelectionChange(selection) {
+                this.ids = selection.map(item => item.)
+                this.single = selection.length !== 1
+                this.multiple = !selection.length
+            },
+            /** 修改按钮操作 */
+            handleUpdate(row) {
+                this.reset()
+                const  =
+                row. || this.ids
+                getSysUserPlan().then(response => {
+                    this.form = response.data
+                    this.open = true
+                    this.title = '修改SysUserPlan'
+                    this.isEdit = true
+                })
+            },
+            /** 提交按钮 */
+            submitForm: function () {
+                this.$refs['form'].validate(valid => {
+                    if (valid) {
+                        if (this.form. !== undefined) {
+                            updateSysUserPlan(this.form).then(response => {
+                                if (response.code === 200) {
+                                    this.msgSuccess(response.msg)
+                                    this.open = false
+                                    this.getList()
+                                } else {
+                                    this.msgError(response.msg)
+                                }
+                            })
+                        } else {
+                            addSysUserPlan(this.form).then(response => {
+                                if (response.code === 200) {
+                                    this.msgSuccess(response.msg)
+                                    this.open = false
+                                    this.getList()
+                                } else {
+                                    this.msgError(response.msg)
+                                }
+                            })
+                        }
+                    }
+                })
+            },
+            /** 删除按钮操作 */
+            handleDelete(row) {
+                var Ids = (row. && [row.]) || this.ids
+
+                this.$confirm('是否确认删除编号为"' + Ids + '"的数据项?', '警告', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(function () {
+                      return delSysUserPlan( { 'ids': Ids })
+                }).then((response) => {
+                   if (response.code === 200) {
+                     this.msgSuccess(response.msg)
+                     this.open = false
+                     this.getList()
+                   } else {
+                     this.msgError(response.msg)
+                   }
+                }).catch(function () {
+                })
+            }
+        }
+    }
+</script>
